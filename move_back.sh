27f0a3c2a04663158dff8f3d34c8148f71583aef
Donny Nadolny <donny.nadolny@gmail.com>
2013-05-07 21:46:16 -0400

    Add in the set-version profile for the ecosystem build script.
    Also adding a "move_back.sh" script to undo the file moving that happens when you run that script

---
+++ move_back.sh
@@ -0,0 +1,4 @@
+mv com.github.dnadolny.javatoscala/META-INF/MANIFEST.MF.original com.github.dnadolny.javatoscala/META-INF/MANIFEST.MF
+mv com.github.dnadolny.javatoscala.tests/META-INF/MANIFEST.MF.original com.github.dnadolny.javatoscala.tests/META-INF/MANIFEST.MF
+mv com.github.dnadolny.javatoscala.source.feature/feature.xml.original com.github.dnadolny.javatoscala.source.feature/feature.xml
+mv com.github.dnadolny.javatoscala.feature/feature.xml.original com.github.dnadolny.javatoscala.feature/feature.xml
