54329df7ca8913294c0582119660a985763db78e
Donny Nadolny <donny.nadolny@gmail.com>
2013-08-21 22:16:30 -0400

    We don't want to have the "delete after conversion" box checked by default

--- com.github.dnadolny.javatoscala/src/com/github/dnadolny/javatoscala/PreferencesInitializer.scala
+++ com.github.dnadolny.javatoscala/src/com/github/dnadolny/javatoscala/PreferencesInitializer.scala
@@ -6,7 +6,6 @@ import com.github.dnadolny.javatoscala.Preferences._
 
 class PreferencesInitializer extends AbstractPreferenceInitializer {
   override def initializeDefaultPreferences(): Unit = {
-    prefs.setDefault(DeleteJavaAfterConversionKey, true)
     prefs.setDefault(AppendJavaAsCommentKey, true)
   }
 }
